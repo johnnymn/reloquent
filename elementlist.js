40153a574dd3f51210ee50a57df698a268909986
Johnny Martinez <johnnymn@hotmail.com>
2016-01-31 11:04:06 -0430

    Documentation added as gh-page-

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Reloquent\\Commands\\GenerateModel"],["c","Reloquent\\Contracts\\ReloquentClientContract"],["c","Reloquent\\Model"],["c","Reloquent\\ReloquentClient"],["c","Reloquent\\ReloquentServiceProvider"]];
